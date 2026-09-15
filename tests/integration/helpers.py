c1928607887ece37b744df6596ea0cff49168a11
Alex Batisse <alex.batisse@canonical.com>
2026-09-15 14:57:09 +0200

    refactor: Move terraform manager to common tests helper

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -3,6 +3,10 @@
 
 """Integration test helpers for Apache Polaris."""
 
+import os
+import shlex
+import shutil
+import subprocess
 from argparse import Namespace
 from pathlib import Path
 from typing import TypedDict
@@ -170,3 +174,36 @@ def set_s3_credentials(
     secret_uri = juju.add_secret("s3-credentials", params)
     juju.grant_secret(secret_uri, s3_app_name)
     juju.config(s3_app_name, {"credentials": secret_uri})
+
+
+class TfDirManager:
+    """Taken from https://github.com/canonical/observability-stack."""
+
+    def __init__(self, base_tmpdir):
+        self.base: str = str(base_tmpdir)
+        self.dir: str = ""
+
+    @property
+    def tf_cmd(self):
+        return f"terraform -chdir={self.dir}"
+
+    def init(self, tf_file: str):
+        """Initialize a Terraform module in a subdirectory."""
+        self.dir = os.path.join(self.base, "terraform")
+        os.makedirs(self.dir, exist_ok=True)
+        shutil.copy(tf_file, os.path.join(self.dir, "main.tf"))
+        subprocess.run(shlex.split(f"{self.tf_cmd} init -upgrade"), check=True)
+
+    @staticmethod
+    def _args_str(target: str | None = None, **kwargs) -> str:
+        target_arg = f"-target module.{target}" if target else ""
+        var_args = " ".join(f"-var {k}={v}" for k, v in kwargs.items())
+        return "-auto-approve " + f"{target_arg} " + var_args
+
+    def apply(self, target: str | None = None, **kwargs):
+        cmd_str = f"{self.tf_cmd} apply " + self._args_str(target, **kwargs)
+        subprocess.run(shlex.split(cmd_str), check=True)
+
+    def destroy(self, **kwargs):
+        cmd_str = f"{self.tf_cmd} destroy " + self._args_str(None, **kwargs)
+        subprocess.run(shlex.split(cmd_str), check=True)
